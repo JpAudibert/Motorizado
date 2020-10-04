a830502472150fd0208949bea3a3b2c552f06d21
João Pedro Basso Audibert <hotsjoao@gmail.com>
2020-10-04 19:21:37 -0300

    adding helpers

---
+++ src/helpers/CombosDAO.java
@@ -0,0 +1,127 @@
+
+package helpers;
+
+import java.sql.ResultSet;
+import javax.swing.JComboBox;
+
+public class CombosDAO {
+
+    ResultSet resultado = null;
+
+    // construtor 1
+    public void popularCombo(String tabela, JComboBox combo) {
+
+        combo.removeAllItems();
+
+        ComboItem item = new ComboItem();
+        item.setCodigo(0);
+        item.setDescricao("Selecione");
+        combo.addItem(item);
+
+        try {
+            resultado = new DBConnection().getConnection().createStatement().executeQuery("select * from " + tabela);
+
+            if (resultado.isBeforeFirst()) {
+                while (resultado.next()) {
+                    item = new ComboItem();
+                    item.setCodigo(resultado.getInt(1));
+                    item.setDescricao(resultado.getString(2));
+
+                    combo.addItem(item);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao popular Combo = " + e.toString());
+        }
+    }
+
+    // construtor 2
+    public void popularCombo(String tabela, String campo1, String campo2, JComboBox combo, String complementoSQL) {
+
+        combo.removeAllItems();
+
+        ComboItem item = new ComboItem();
+        item.setCodigo(0);
+        item.setDescricao("Selecione");
+        combo.addItem(item);
+
+        try {
+            resultado = new DBConnection().getConnection().createStatement().executeQuery("select * from " + tabela + " " + complementoSQL);
+
+            if (resultado.isBeforeFirst()) {
+                while (resultado.next()) {
+                    item = new ComboItem();
+                    item.setCodigo(resultado.getInt(campo1));
+                    item.setDescricao(resultado.getString(campo2));
+
+                    combo.addItem(item);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao popular Combo = " + e.toString());
+        }
+    }
+
+    public void popularCombo(JComboBox combo) {
+
+        combo.removeAllItems();
+
+        ComboItem item = new ComboItem();
+        item.setCodigo(0);
+        item.setDescricao("Selecione");
+        combo.addItem(item);
+
+        try {
+            ComboItem[] items = new ComboItem[10];
+
+            for (int i = 1; i <= items.length; i++) {
+                item = new ComboItem();
+                item.setCodigo(i);
+                item.setDescricao(i + "");
+
+                if (i == 10) {
+                    item.setDescricao(i + " ou +");
+                }
+
+                combo.addItem(item);
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao popular Combo = " + e.toString());
+        }
+    }
+
+    public void popularComboConcat(String tabela, String campo1, String campo2, String union, String concat, JComboBox combo, String complementoSQL) {
+
+        combo.removeAllItems();
+
+        ComboItem item = new ComboItem();
+        item.setCodigo(0);
+        item.setDescricao("Selecione");
+        combo.addItem(item);
+
+        try {
+            resultado = new DBConnection().getConnection().createStatement().executeQuery("select * from " + tabela + " " + complementoSQL);
+
+            if (resultado.isBeforeFirst()) {
+                while (resultado.next()) {
+                    item = new ComboItem();
+                    item.setCodigo(resultado.getInt(campo1));
+                    item.setDescricao(resultado.getString(campo2) + union + resultado.getString(concat));
+
+                    combo.addItem(item);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao popular Combo = " + e.toString());
+        }
+    }
+
+    public void definirItemCombo(JComboBox combo, ComboItem item) {
+        for (int i = 0; i < combo.getItemCount(); i++) {
+            if (((ComboItem) combo.getItemAt(i)).getCodigo() == (item.getCodigo())) {
+                combo.setSelectedIndex(i);
+                return;
+            }
+        }
+    }
+}
---
+++ src/helpers/Inserts.java
@@ -0,0 +1,78 @@
+package helpers;
+
+import java.sql.Statement;
+
+
+public class Inserts {
+    public static void insertStates(){
+        try {
+            Statement stmt = DBConnection
+                    .getInstance()
+                    .getConnection()
+                    .createStatement();
+            
+            String sql = " "
+                    + "INSERT INTO state VALUES (DEFAULT, 'Acre', 'AC'),"
+                    + "(DEFAULT, 'Alagoas', 'AL'),"
+                    + "(DEFAULT, 'Amapá', 'AP'),"
+                    + "(DEFAULT, 'Amazonas', 'AM'),"
+                    + "(DEFAULT, 'Bahia', 'BA'),"
+                    + "(DEFAULT, 'Ceará', 'CE'),"
+                    + "(DEFAULT, 'Distrito Federal', 'DF'),"
+                    + "(DEFAULT, 'Espírito Santo', 'ES'),"
+                    + "(DEFAULT, 'Goiás', 'GO'),"
+                    + "(DEFAULT, 'Maranhão', 'MA'),"
+                    + "(DEFAULT, 'Mato Grosso', 'MT'),"
+                    + "(DEFAULT, 'Mato Grosso do Sul', 'MS'),"
+                    + "(DEFAULT, 'Minas Gerais', 'MG'),"
+                    + "(DEFAULT, 'Pará', 'PA'),"
+                    + "(DEFAULT, 'Paraíba', 'PB'),"
+                    + "(DEFAULT, 'Paraná', 'PR'),"
+                    + "(DEFAULT, 'Pernambuco', 'PE'),"
+                    + "(DEFAULT, 'Piauí', 'PI'),"
+                    + "(DEFAULT, 'Rio de Janeiro', 'RJ'),"
+                    + "(DEFAULT, 'Rio Grande do Norte', 'RN'),"
+                    + "(DEFAULT, 'Rio Grande do Sul', 'RS'),"
+                    + "(DEFAULT, 'Rondônia', 'RO'),"
+                    + "(DEFAULT, 'Roraima', 'RR'),"
+                    + "(DEFAULT, 'Santa Catarina', 'SC'),"
+                    + "(DEFAULT, 'São Paulo', 'SP'),"
+                    + "(DEFAULT, 'Sergipe', 'SE'),"
+                    + "(DEFAULT, 'Tocantins', 'TO')";
+            
+            if(stmt.execute(sql)){
+                System.out.println("Estados Inseridos.");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void insertQualifications(){
+        try {
+            Statement stmt = DBConnection
+                    .getInstance()
+                    .getConnection()
+                    .createStatement();
+            
+            String sql = " "
+                    + " INSERT INTO qualification VALUES "
+                    + " (DEFAULT, 'Montagem/manutenção de computadores'),"
+                    + " (DEFAULT, 'Programação de softwares'),"
+                    + " (DEFAULT, 'Primeiros socorros'),"
+                    + " (DEFAULT, 'Mêcaninca automotiva'),"
+                    + " (DEFAULT, 'Elétrica automotiva'),"
+                    + " (DEFAULT, 'Mecânica industrial'),"
+                    + " (DEFAULT, 'Elétrica industrial'),"
+                    + " (DEFAULT, 'Experiência na área de alimentos'),"
+                    + " (DEFAULT, 'É/foi escoteiro ou  bandeirante'),"
+                    + " (DEFAULT, 'Já trabalhou em escritório')";
+            
+            if(stmt.execute(sql)){
+                System.out.println("Qualificações Inseridas.");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
